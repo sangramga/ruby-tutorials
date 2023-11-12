0b1379dbfca0c56aa1cc8d245d572e6e9a64ae8b
sangramga <sangramga@gmail.com>
2023-11-12 22:41:13 +0530

    Ruby Hello World

---
+++ main.rb
@@ -0,0 +1,15 @@
+puts 'Hello, World!'
+
+names = Array.new
+names[0] = "Sam" 
+names[1] = "Priya"
+names[2] = "Sarah"
+
+hmap = {
+    "Sam" => 25.5,
+    "Priya" => 79.23,
+    "Sarah" => 60.34
+}
+print (names)
+puts (names)
+puts (hmap)
